e3e610ff57fb5a31100e2dbbbf7fb2b2b6abe036
Andrey885 <asshilov@yandex.com>
2020-06-04 19:01:02 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-wget https://drive.google.com/uc?id=1d-Q-tzGo1RN_YbmvMP5gArQZZdSnclJ0 -O checkpoints/en_de_final.pt
+wget https://drive.google.com/uc?id=1rNYfjFcSnp3Mi5sv0CL4Q9w6lQVlxhMh -O checkpoints/en_de_final.pt
 python3 -m pip install -r requirements.txt
 sudo python3 -m spacy download en
 sudo python3 -m spacy download de_core_news_sm
