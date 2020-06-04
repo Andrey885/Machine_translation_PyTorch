4fdda57937c29c1f3c30727111f4051335c6ff1e
AShilov2 <Andrey.Shilov2@harman.com>
2020-06-04 15:37:47 +0300

    minor fixes

---
+++ setup.sh
@@ -0,0 +1,6 @@
+wget https://drive.google.com/uc?id=1d-Q-tzGo1RN_YbmvMP5gArQZZdSnclJ0 -O checkpoints/en_de_final.pt
+python3 -m pip install -r requirements.txt
+sudo python3 -m spacy download en
+sudo python3 -m spacy download de_core_news_sm
+git submodule init
+git submodule update
