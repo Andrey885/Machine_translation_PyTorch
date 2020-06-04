145b7898a6d66e847ff7cf39fef292b0765c83d8
Shilov <Andrey.Shilov2@harman.com>
2020-06-04 15:15:28 +0300

    minor fixes

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ wget https://drive.google.com/uc?id=1d-Q-tzGo1RN_YbmvMP5gArQZZdSnclJ0 -O checkpo
 python3 -m pip install -r requirements.txt
 sudo python3 -m spacy download en
 sudo python3 -m spacy download de_core_news_sm
+git submodule init
+git submodule update
